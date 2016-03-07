7a93cd517dab763b7ba34f7e9df88bf178b3549f
adriana <adrianamolello@gmail.com>
2016-03-07 17:52:13 +0000

    Added new section page and button/links

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Flixter::Application.routes.draw do
   devise_for :users
   resources :courses, only: [:index, :show]
   namespace :instructor do
-     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create] do
-        resources :lessons, only: [:new, :create]
-      end
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
     end
   end
     # The priority is based upon order of creation: first created -> highest priority.
