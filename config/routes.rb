93b2b833d2a78b4836d94394f774fbcf0580a5ec
adriana <adrianamolello@gmail.com>
2016-03-15 20:09:33 +0000

    hooking up mvc for sections java

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Flixter::Application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :sections, only: [:update]
     resources :lessons, only: [:update]
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
+
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
